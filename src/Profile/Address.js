6d9cb21f62ad93904909b35f7a414dcee7a55714
Oussama Kmar <YOUR EMAILoussamakmar1@gmail.com>
2023-05-29 17:50:33 +0100

    first cimmit

---
+++ src/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div>16 cité werfelli</div>
+  )
+}
+
+export default Address
---
+++ src/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Oussama from './1635351071137.jpeg'
+
+const ProfilePhoto = () => {
+  return (
+    <div><img src={Oussama} alt="Logo" Height="300px" /></div>
+  )
+}
+
+export default ProfilePhoto
